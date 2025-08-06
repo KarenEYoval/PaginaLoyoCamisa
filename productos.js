f18074b174b0000e32c8473aa933de6d07aed63d
Karen Yoval <karenyoval26@gmail.com>
2025-08-05 22:20:16 -0600

    Version 2

---
+++ productos.js
@@ -0,0 +1,137 @@
+const productos = [
+  {
+    nombre: "1",
+    precio: 280,
+    imagen: "Imagenes/BossBlanca.jpg"
+  },
+  {
+    nombre: "2",
+    precio: 300,
+    imagen: "Imagenes/BossNegra.jpg"
+  },
+  {
+    nombre: "3",
+    precio: 300,
+    imagen: "Imagenes/BossNegra2.jpg"
+  },
+  {
+    nombre: "4",
+    precio: 300,
+    imagen: "Imagenes/BossRoja.jpg"
+  },
+  {
+    nombre: "5",
+    precio: 300,
+    imagen: "Imagenes/GuessBlanca.jpg"
+  },
+  {
+    nombre: "6",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0061.jpg"
+  },
+  {
+    nombre: "7",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0062.jpg"
+  },
+  {
+    nombre: "8",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0063.jpg"
+  },
+  {
+    nombre: "9",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0064.jpg"
+  },
+  {
+    nombre: "10",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0065.jpg"
+  },
+  {
+    nombre: "11",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0066.jpg"
+  },
+  {
+    nombre: "12",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0067.jpg"
+  },
+  {
+    nombre: "13",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0069.jpg"
+  },
+  {
+    nombre: "14",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0071.jpg"
+  },
+  {
+    nombre: "15",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0072.jpg"
+  },
+  {
+    nombre: "16",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0073.jpg"
+  },
+  {
+    nombre: "17",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0074.jpg"
+  },
+  {
+    nombre: "18",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0075.jpg"
+  },
+  {
+    nombre: "19",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0076.jpg"
+  },
+  {
+    nombre: "20",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0077.jpg"
+  },
+  {
+    nombre: "21",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0078.jpg"
+  },
+  {
+    nombre: "22",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0079.jpg"
+  },
+  {
+    nombre: "23",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0080.jpg"
+  },
+  {
+    nombre: "24",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0081.jpg"
+  },
+  {
+    nombre: "25",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0082.jpg"
+  },
+  {
+    nombre: "26",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0083.jpg"
+  },
+  {
+    nombre: "27",
+    precio: 300,
+    imagen: "Imagenes/IMG-20250805-WA0084.jpg"
+  }
+];
